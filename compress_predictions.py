0297424f37fcc0778e66dc960915973d2dc1532c
William Hill <whill4@me.com>
2022-08-23 16:56:41 +0200

    changed experiments file structure and added decompression functionality

--- compress_predictions.py
+++ compress_predictions.py
@@ -1,11 +1,36 @@
 import zipfile
 import os
+import sys
 
 original_name = "generated_predictions.txt"
 zipped_name = "generated_predictions.txt.zip"
 
 
-def main():
+def compress(dirpath, filenames):
+    # check if a generate_predictions.txt file is present
+    if (original_name in filenames) and (zipped_name not in filenames):
+        # need to compress this file
+        print(f"Compressing {dirpath}/{original_name}...")
+        zipped = zipfile.ZipFile(f"{dirpath}/{zipped_name}", "w")
+        zipped.write(
+            f"{dirpath}/{original_name}",
+            original_name,
+            compress_type=zipfile.ZIP_DEFLATED,
+        )
+        zipped.close()
+
+
+def uncompress(dirpath, filenames):
+    # check if a generate_predictions.txt file is present
+    if zipped_name in filenames:
+        # need to compress this file
+        print(f"Uncompressing {dirpath}/{zipped_name}...")
+        zipped = zipfile.ZipFile(f"{dirpath}/{zipped_name}", "r")
+        zipped.extractall(f"{dirpath}")
+        zipped.close()
+
+
+def main(compression_type):
     # loop through all of the experiment folders (excluding checkpoints)
     for (dirpath, dirnames, filenames) in os.walk("./experiments", topdown=True):
         # don't process checkpoints
@@ -13,17 +38,10 @@ def main():
         for idx in del_idx[::-1]:
             del dirnames[idx]
 
-        # check if a generate_predictions.txt file is present
-        if (original_name in filenames) and (zipped_name not in filenames):
-            # need to compress this file
-            print(f"Compressing {dirpath}/{original_name}...")
-            zipped = zipfile.ZipFile(f"{dirpath}/{zipped_name}", "w")
-            zipped.write(
-                f"{dirpath}/{original_name}",
-                original_name,
-                compress_type=zipfile.ZIP_DEFLATED,
-            )
-            zipped.close()
+        if compression_type == "compress":
+            compress(dirpath, filenames)
+        else:
+            uncompress(dirpath, filenames)
 
         # don't recurse into files
         del filenames[:]
@@ -32,4 +50,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # check the sysargs
+    options = ["compress", "uncompress"]
+    if len(sys.argv) != 2 or sys.argv[1] not in options:
+        print(f"Usage: {sys.argv[0]} <compression type: {options[0]} or {options[1]}>")
+        exit()
+
+    main(sys.argv[1])
