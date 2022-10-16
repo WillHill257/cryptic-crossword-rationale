81662ece42a082360754de72457e6c2f58ff74ba
William Hill <whill4@me.com>
2022-10-16 21:23:49 +0200

    copied ir->o data to data directory

---
+++ copy_generated_predictions.py
@@ -0,0 +1,32 @@
+"""
+copy the generated predictions (from the experimental output to the data folder)
+"""
+
+import os
+import shutil
+
+if __name__ == "__main__":
+    # define the path components
+    experimental_path = "./experiments/i_or_data"
+    data_path = "./cryptics/data/json/generated-predictions"
+    dataset_names = ["random", "naive-disjoint", "word-initial-disjoint"]
+    models = ["t5-small", "t5-large"]
+    splits = ["train", "validation", "test"]
+    rationale_filename = "generated_predictions.json"
+
+    # loop through all the files
+    for dataset in dataset_names:
+        for model in models:
+            for split in splits:
+                # determine the source and destinations, with the filenames
+                source_path = f"{experimental_path}/{dataset}/{model}/{split}/{rationale_filename}"
+                destination_path = f"{data_path}/{dataset}/{model}/{split}.json"
+
+                print(f"Copying {source_path} to {destination_path}")
+
+                # make sure the destination directory exists
+                i = len(destination_path) - destination_path[::-1].find("/")
+                os.makedirs(os.path.dirname(destination_path[:i]), exist_ok=True)
+
+                # copy the file
+                shutil.copyfile(source_path, destination_path)
